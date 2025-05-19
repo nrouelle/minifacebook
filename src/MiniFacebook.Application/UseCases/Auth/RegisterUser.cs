28f5683525e9c43e70f5377c7eead227915312ca
Nadège Rouelle <nadege@rouelleconsulting.fr>
2025-05-19 21:04:33 +0200

    add usecases

---
+++ src/MiniFacebook.Application/UseCases/Auth/RegisterUser.cs
@@ -0,0 +1,32 @@
+﻿using MiniFacebook.Domain.Entities;
+using Org.BouncyCastle.Crypto.Generators;
+using MiniFacebook.Domain.Interfaces;
+
+namespace MiniFacebook.Application.UseCases.Auth
+{
+    public class RegisterUser
+    {
+        private readonly IUserRepository _userRepository;
+
+        public RegisterUser(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> ExecuteAsync(string fullName, string email, string password)
+        {
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+            var user = new User
+            {
+                FullName = fullName,
+                Email = email,
+                PasswordHash = hash
+            };
+
+            await _userRepository.CreateAsync(user);
+            
+            return user;
+        }
+    }
+
+}
---
+++ src/MiniFacebook.Application/UseCases/Likes/LikePost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniFacebook.Application.UseCases.Likes
+{
+    internal class LikePost
+    {
+    }
+}

1b7c65688b395d5b8494a2fe39cd1ed3129e5df4
Nadege ROUELLE <nadege.rouelle.ext@careside.care>
2025-05-20 09:36:01 +0200

    add jwt package and add middleware

--- src/MiniFacebook.API/Program.cs
+++ src/MiniFacebook.API/Program.cs
@@ -5,6 +5,9 @@ using MiniFacebook.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using MiniFacebook.Domain.Interfaces;
 using MiniFacebook.API.Endpoints;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,12 +17,36 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 //builder.Services.AddDbContext<AppDbContext>(options =>
 //    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var config = builder.Configuration;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = config["Jwt:Issuer"],
+            ValidAudience = config["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(config["Jwt:Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 
 // DI
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<AuthService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapAuthEndpoints();
 app.MapPostEndpoints();

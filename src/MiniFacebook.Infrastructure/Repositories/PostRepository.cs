fba089ecd20215da39263dd6f33373f7face243d
Nadège Rouelle <nadege@rouelleconsulting.fr>
2025-05-20 14:22:46 +0200

    create addpost method in repository

---
+++ src/MiniFacebook.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,22 @@
+﻿using MiniFacebook.Domain.Entities;
+using MiniFacebook.Domain.Interfaces;
+using MiniFacebook.Infrastructure.Data;
+
+namespace MiniFacebook.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PostRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

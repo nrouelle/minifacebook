52659f3204c4422d020b5ba2e5197da766b9f5a6
Nadège Rouelle <nadege@rouelleconsulting.fr>
2025-05-21 19:19:21 +0200

    add getallposts integration test

---
+++ src/MiniFacebook.Tests/IntegrationTests/GetAllPostsIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniFacebook.Application.UseCases.Posts;
+using MiniFacebook.Domain.Entities;
+using MiniFacebook.Infrastructure.Data;
+using MiniFacebook.Infrastructure.Repositories;
+
+namespace MiniFacebook.Tests.IntegrationTests
+{
+    public class GetAllPostsIntegrationTests
+    {
+        [Fact]
+        public async Task ExecuteAsync_ShouldReturnInsertedPosts_FromDatabase()
+        {
+            // Arrange : base InMemory
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb_Posts")
+                .Options;
+
+            using var context = new AppDbContext(options);
+            var post1 = new Post(Guid.NewGuid(), Guid.NewGuid(), "Post 1", DateTime.UtcNow);
+            var post2 = new Post(Guid.NewGuid(), Guid.NewGuid(), "Post 2", DateTime.UtcNow);
+
+            var repo = new PostRepository(context);
+            await repo.AddAsync(post1);
+            await repo.AddAsync(post2);
+            var useCase = new GetAllPosts(repo);
+
+            // Act
+            var result = (await useCase.ExecuteAsync()).ToList();
+
+            // Assert
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, p => p.Content == "Post 1");
+            Assert.Contains(result, p => p.Content == "Post 2");
+        }
+    }
+}

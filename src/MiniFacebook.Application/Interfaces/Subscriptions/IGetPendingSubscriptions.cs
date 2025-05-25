25317ed45fb85fbf2280e7025ea6010cdbd0f49d
Nadège Rouelle <nadege@rouelleconsulting.fr>
2025-05-25 14:42:41 +0200

    add GetPendingSubscriptions use case

---
+++ src/MiniFacebook.Application/Interfaces/Subscriptions/IGetPendingSubscriptions.cs
@@ -0,0 +1,9 @@
+﻿using MiniFacebook.Application.DTOs.Subscriptions;
+
+namespace MiniFacebook.Application.Interfaces.Subscriptions
+{
+    public interface IGetPendingSubscriptions
+    {
+        Task<List<SubscriptionDto>> ExecuteAsync(string userEmail);
+    }
+}
